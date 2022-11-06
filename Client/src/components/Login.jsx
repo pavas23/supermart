cece10b8f7f67b8b5de0823af02ad4a45cf265e9
Pavas Garg <pavasgarg2003@gmail.com>
2022-11-07 01:08:21 +0530

    added login page frontend

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -2,12 +2,15 @@ import "../css/login.css";
 export default function Login() {
     return (
         <>
-            <form method = "POST" action="http://127.0.0.1:5000/user/login/submit">
-                <input type="text" name="name" id="input_name" placeholder="Enter your name" />
-                <input type="text" name="email" id="input_email" placeholde="Enter your user ID" />
-                <input type="password" name="password" id="input_password" placeholder="Enter your password" />
-                <input type = "submit" value="Login"/>
-            </form>
+            <div id="login">
+                <h1>Login to Online Supermarket</h1>
+                <form method="POST" action="http://127.0.0.1:5000/user/login/submit">
+                    <input type="text" name="name" id="input_name" placeholder="Enter your name" />
+                    <input type="text" name="email" id="input_email" placeholder="Enter your user ID" />
+                    <input type="password" name="password" id="input_password" placeholder="Enter your password" />
+                    <input type="submit" value="Login" id="login-btn" />
+                </form>
+            </div>
         </>
     );
 }
