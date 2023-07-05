8d5c2fb3825e535943a9be7ee1e8cb34c8a79c57
Pavas Garg <pavasgarg2003@gmail.com>
2023-07-06 01:43:24 +0530

    Update Register.jsx

--- src/components/Customer/Register.jsx
+++ src/components/Customer/Register.jsx
@@ -48,7 +48,6 @@ export default function Login(props) {
     }
 
     async function addCustomer() {
-      console.log(`${REACT_APP_APIURL}`);
       var response = await fetch(`${REACT_APP_APIURL}/customer/add`, {
         method: "POST",
         headers: {
